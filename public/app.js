7bb154a3d1588619a2c76bc18b6af5457d1ebe85
Ayana <ayana.d.i.wilson@gmail.com>
2015-10-19 12:54:59 -0400

    removed serialize method on defaults object

--- public/app.js
+++ public/app.js
@@ -10,17 +10,17 @@ var app = angular.module('jsdataWorkshop', ['js-data', 'ui.router'])
     DSProvider.defaults.basePath = '/api';
     DSProvider.defaults.idAttribute = '_id';
 
-    /*
-    removes the relations from an object before the request is sent to the DB
-    */
-    DSProvider.defaults.serialize = function (resource, data) {
-      if (resource.relationFields && resource.relationFields.length){
-        data = angular.copy(data);
-        resource.relationFields.forEach(function(relation){
-          if(data[relation] && typeof data[relation] === 'object') data[relation] = data[relation]._id
-        })
-      }
-    }
+    // /*
+    // removes the relations from an object before the request is sent to the DB
+    // */
+    // DSProvider.defaults.serialize = function (resource, data) {
+    //   if (resource.relationFields && resource.relationFields.length){
+    //     data = angular.copy(data);
+    //     resource.relationFields.forEach(function(relation){
+    //       if(data[relation] && typeof data[relation] === 'object') data[relation] = data[relation]._id
+    //     })
+    //   }
+    // }
 
   })
   
